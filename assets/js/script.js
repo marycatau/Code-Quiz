570d1736c4b4a1853b2d3e7a30a7c1acd424831b
marycatau <marycatherine.au@gmail.com>
2022-07-25 14:04:57 +0100

    upload the trial version

---
+++ assets/js/script.js
@@ -0,0 +1,190 @@
+// Get references to button element in step 1
+var startBtn = document.querySelector("#startbtn");
+
+
+//Get references to the question content in step 2
+var NextBtn = document.querySelector("#NextQuest");
+
+var questNo = document.querySelector("#QuestNo");
+var QuestDetail = document.querySelector("#QuestDetail");
+var ansA = document.querySelector("#ansA");
+var ansB = document.querySelector("#ansB");
+var ansC = document.querySelector("#ansC");
+var ansD = document.querySelector("#ansD");
+
+var userAns ;
+var AnsStatus = document.querySelector("#AnsStatus");
+
+
+//Get ref and variable for timer in step 2
+var timeEl = document.querySelector("#TimerCount");
+var secondsLeft = 30;
+
+//set variable for quiz score and ongoing question counter in step 2 and 3
+var questStatus=0;
+var score=0;
+
+//set reference in step 3 -- enter initial
+var userScore=document.querySelector("#Score");
+var userName= document.querySelector("#username");
+
+var submitbtn = document.querySelector("#Submit");
+
+
+// Question details
+const questions = [];
+questions[0] = {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answerOption: ["<script>", "<scripting>", "<js>", "<javascript>",] ,
+    ans:"A",
+};
+
+
+questions[1] = {
+    question: "What is the correct JavaScript syntax to change the content of the HTML element below? <p id='demo'>This is a demonstration.</p>",
+    answerOption: [" document.getElement('id').innerHTML = 'Hello World!'" , " #demo.innerHTML = 'Hello World!'" , "document.querySelector.innerHTML ='Hello World'" , "document.getElementById('demo').innerHTML = 'Hello World'",],
+    ans:"D",
+};
+
+
+
+function init(){
+    hideElement('step2');
+    hideElement('step3');
+  }
+
+
+function SetTimer(){
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+    
+        if(secondsLeft === 0) {
+          // Stops execution of action at set interval
+          clearInterval(timerInterval);
+          EndofQuiz();
+          // Calls function to create and append image
+          //sendMessage();
+        }
+    
+      }, 1000);
+}
+
+function ShowQuestionPage(){
+    hideElement('step1');
+    hideElement('step3');
+    showElement('step2');
+    SetTimer();
+    ShowQuestion();
+
+}
+
+ //show questions details   
+function ShowQuestion(){   
+    questNo = questStatus;
+    QuestDetail.textContent = questions[questStatus].question;
+    ansA.textContent = questions[questStatus].answerOption[0];
+    ansB.textContent = questions[questStatus].answerOption[1];
+    ansC.textContent = questions[questStatus].answerOption[2];
+    ansD.textContent = questions[questStatus].answerOption[3];
+    
+}
+
+function checkAns(){
+ 
+
+    var ans = document.getElementsByName('Ans');
+
+    for (let i of ans) {
+
+        if (i.checked) {
+            userAns= i.value;
+        }
+    }
+    console.debug(userAns);
+    console.debug(questions[questStatus].ans);
+    console.debug(questStatus)
+
+
+    if (userAns === questions[questStatus].ans){
+        AnsStatus.textContent = "Correct";
+        score += 1;
+        console.debug(score);
+    }
+    else{
+        AnsStatus.textContent = "Oh! Wrong Answer."
+        secondsLeft -= 5;
+    }  
+   
+   questStatus=questStatus+1; 
+   
+   if(questStatus<questions.length) {
+        ShowQuestion();
+    }
+    else
+    {
+        questStatus = 0;
+        secondsLeft = 1;
+        EndofQuiz();
+    }
+
+
+}
+
+
+
+
+
+function EndofQuiz(){
+    hideElement('step1');
+    hideElement('step2');
+    showElement('step3');
+    userScore.textContent = score;
+}
+
+
+
+function enterInital(){
+    var storedScore = JSON.parse(localStorage.getItem("StoredScore"));
+    console.debug(storedScore);
+    var ThisScore = {
+        name: userName.value,
+        score: score,
+    }
+    
+    if(!Array.isArray(storedScore)){
+        storedScore = [];
+    }
+
+    console.debug(ThisScore);    
+    storedScore.push(ThisScore);  
+    
+    console.debug(storedScore);
+    localStorage.setItem("StoredScore", JSON.stringify(storedScore));
+
+    
+    window.location.href = "./HighestScore.html"
+
+}
+
+
+function hideElement(id){
+    document.getElementById(id).style.display = "none";
+}
+
+function showElement(id){
+    document.getElementById(id).style.display = 'block';
+}
+
+
+
+
+
+startBtn.addEventListener("click", ShowQuestionPage);
+NextBtn.addEventListener("click", checkAns);
+submitbtn.addEventListener("click", enterInital);
+//generateBtn.addEventListener("click", step2ft);
+//NextCriteriaBtn.addEventListener("click", step3ft);
+//NextLengthBtn.addEventListener("click", step4ft);
+//SubmitBtn.addEventListener("click", writePassword);
+init();
---
+++ assets/js/script2.js
@@ -0,0 +1,44 @@
+var HighestScore;
+var scoreList = document.querySelector("#ScoreList");
+var clearbtn = document.querySelector("#ClearScore");
+
+
+var ThisScore = [];
+var ScoreRecord=[];
+var length;
+var StoredScore= [];
+
+function showRecord(StoredScore){
+    for(i=0; i<StoredScore.length; i++){
+        var ScoreRecord = StoredScore[i];
+        var li = document.createElement("li");
+        li.textContent = ScoreRecord.name +"     SORES      "+ ScoreRecord.score;
+        li.setAttribute("data-index", i);
+
+        scoreList.appendChild(li);
+       
+    }
+
+}
+
+function init(){ 
+    StoredScore = JSON.parse(localStorage.getItem("StoredScore"));
+    //length = StoredScore.length
+    console.debug(StoredScore);
+    console.debug(typeof(StoredScore));
+    console.debug(StoredScore.length);
+    console.debug(scoreList);
+    //console.debug(ScoreRecord);
+    //nameList.textContent = ThisScore.name;
+    //scoreList.textContent = ThisScore.score;
+    showRecord(StoredScore);
+}
+
+function clearRecord(){
+    scoreList.textContent = "";
+    var StoredScore = "";
+    localStorage.setItem("StoredScore", JSON.stringify(StoredScore));
+}
+
+clearbtn.addEventListener("click", clearRecord);
+init();
